52d75680a71835be652c080f6df9437c4b128c9d
hao wang <ge_nah@hotmail.com>
2019-05-08 05:51:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  number = int
+  while number >= 0
+    puts "#{number}SECOND(S)!"
+    number -= 1 
+  end
+  puts "HAPPY NEW YEAR!"
 end
