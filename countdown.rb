a364f71d1bc93c726f0c83d420cf7b669870f4b3
hao wang <ge_nah@hotmail.com>
2019-05-08 06:00:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(int)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  number = int
+  while number >= 0
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
